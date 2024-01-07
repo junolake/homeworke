057c0d7031d315f96badc8319b445e24436173fa
gos-uslugi <imagenuxt@gmail.com>
2024-01-07 21:05:33 +0200

    5th commit

--- a.js
+++ a.js
@@ -1,11 +1,27 @@
 let topItems = document.querySelectorAll('.top100-item');
+let descButton = document.querySelector('#description');
+let descText = document.querySelector('#desc-text');
+let descSpan = document.querySelector('.info-item span');
+
+descButton.addEventListener('click', showDesc);
 
 topItems .forEach(function(item) {
     item.addEventListener("click", function() {
-        // Remove the "color" class from all elements
+        
         topItems.forEach(function(innerItem) {
             innerItem.classList.remove("top-selected");
         });
         item.classList.add("top-selected");
     });
 }); 
+
+function showDesc() {
+    if(descText.classList.contains("off-description")) {
+        descText.classList.remove("off-description");
+        descSpan.classList.add("desc-active");
+    }
+    else {
+        descText.classList.add("off-description");
+        descSpan.classList.remove("desc-active");
+    }
+}
