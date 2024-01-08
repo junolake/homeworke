05b30c8a9fe5298fb283019a6b929322da0ed120
junolake <junolakeofficial@gmail.com>
2024-01-08 09:16:54 +0200

    7th commit fixed js buttons

--- a.js
+++ a.js
@@ -1,19 +1,8 @@
-let topItems = document.querySelectorAll('.top100-item');
 let descButton = document.querySelector('#description');
 let descText = document.querySelector('#desc-text');
 let descSpan = document.querySelector('.info-item span');
 
 descButton.addEventListener('click', showDesc);
-
-topItems .forEach(function(item) {
-    item.addEventListener("click", function() {
-        
-        topItems.forEach(function(innerItem) {
-            innerItem.classList.remove("top-selected");
-        });
-        item.classList.add("top-selected");
-    });
-}); 
 
 function showDesc() {
     if(descText.classList.contains("off-description")) {
